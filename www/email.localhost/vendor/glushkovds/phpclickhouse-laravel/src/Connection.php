46e6b762197e0375e1b26a6f1a8f73a1013fcf82
dukinm <dukinm@gmail.com>
2022-03-11 14:20:55 +0300

    Директория Vendor и файл окружения .env вынесены из gitignore

---
+++ www/email.localhost/vendor/glushkovds/phpclickhouse-laravel/src/Connection.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace PhpClickHouseLaravel;
+
+
+use ClickHouseDB\Client;
+
+class Connection extends \Illuminate\Database\Connection
+{
+
+    /** @var Client */
+    protected $client;
+
+    /**
+     * @return Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param array $config
+     * @return static
+     */
+    public static function createWithClient($config)
+    {
+        $conn = new static(null, $config['database'], '', $config);
+        $conn->client = new Client($config);
+        $conn->client->database($config['database']);
+        $conn->client->setTimeout($config['timeout_query']);
+        $conn->client->setConnectTimeOut($config['timeout_connect']);
+        if ($configSettings =& $config['settings']) {
+            $settings = $conn->getClient()->settings();
+            foreach ($configSettings as $sName => $sValue) {
+                $settings->set($sName, $sValue);
+            }
+        }
+        if ($retries = (int)($config['retries'] ?? null)) {
+            $curler = new CurlerRollingWithRetries();
+            $curler->setRetries($retries);
+            $conn->client->transport()->setDirtyCurler($curler);
+        }
+        return $conn;
+    }
+
+    /** @inheritDoc */
+    protected function getDefaultQueryGrammar()
+    {
+        return new QueryGrammar();
+    }
+
+    /** @inheritDoc */
+    protected function getDefaultSchemaGrammar()
+    {
+        return new SchemaGrammar();
+    }
+
+    /** @inheritDoc */
+    public function getSchemaBuilder()
+    {
+        if (is_null($this->schemaGrammar)) {
+            $this->useDefaultSchemaGrammar();
+        }
+
+        return new SchemaBuilder($this);
+    }
+
+    /** @inheritDoc */
+    public function select($query, $bindings = [], $useReadPdo = true)
+    {
+        $query = QueryGrammar::prepareParameters($query);
+        return $this->client->select($query, $bindings)->rows();
+    }
+
+    /** @inheritDoc */
+    public function statement($query, $bindings = [])
+    {
+        return !$this->client->write($query, $bindings)->isError();
+    }
+
+    /** @inheritDoc */
+    public function affectingStatement($query, $bindings = [])
+    {
+        return (int)$this->statement($query, $bindings);
+    }
+
+}
